c4401508ffe1913dabd21eb6af88b56a1e34b4c4
yuqihu1103 <hu.yuqi@northeastern.edu>
2024-03-15 22:50:29 -0700

    style homepage

--- public/js/index.js
+++ public/js/index.js
@@ -9,117 +9,37 @@ function loadHomepage() {
   const contactInfo = document.getElementById("contact-info");
 
   mainContent.innerHTML = `
-        <h1 class="text-center">Welcome to the Eco Name</h1>
-        <div class="container">
-            <div class="row justify-content-center">
-            <div class="col-lg-6 col-md-12">
-                <img
-                src="./assets/homepage_main_photo.jpg"
-                alt="Artwork illustrating positive mental health"
-                class="img-fluid"
-                />
-            </div>
-            <div class="col-lg-6 col-md-12">
-                <p>
-                Eco game
-                </p>
-                <p>
-                for kids
-                </p>
-            </div>
-            </div>
-        </div>
+  <div class="container">
+  <div class="column">
+      <!-- Content for the first column -->
+      We need you!
+      Protect the Earth by Playing the Games!
+  </div>
+
+  <div class="column">
+      <!-- Content for the second column -->
+      <h1 class="text-center">Planet Protector</h1>
+      <div class="container">
+          <div class="row justify-content-center">
+              <p>
+              The world is in DANGER!
+              </p>
+              <p>
+              It need your help.
+              </p>
+          </div>
+      </div>
+  </div>
+
+  <div class="column">
+      <!-- Content for the third column -->
+      I Am Global Warming.
+      I Am Going to Destroy Your Eco-System.
+  </div>
+</div>
     `;
 
   contactInfo.innerHTML = `
         <h3>Contact the Authors</h3>
     `;
-}
-
-function loadLearnMore() {
-  const mainContent = document.getElementById("main-content");
-  const contactInfo = document.getElementById("contact-info");
-
-  mainContent.innerHTML = `
-        <h1>More About the Eco Game</h1>
-        <div class="row">
-            <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="card" style="width: 18rem">
-                <img
-                src="./assets/learn_more_details.jpg"
-                class="card-img-top"
-                alt="Image illustration of unlocking one's mind"
-                height="180"
-                />
-                <div class="card-body">
-                <h5 class="card-title">How Does It Work?</h5>
-                <p class="card-text">
-                    After registering and loginning in with your cresentials, you
-                    can measure your anxiety and depression level with profession
-                    psychometric instruments. You can view the history of your test
-                    results and apply filters to gain insight if you would like.
-                </p>
-                <p class="card-text">
-                    Do so while enjoying our responsive and interactive UI as well
-                    as secure storage of sensitive data and indormation!
-                </p>
-                </div>
-            </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="card" style="width: 18rem">
-                <img
-                src="./assets/learn_more_anxiety.webp"
-                class="card-img-top"
-                alt="Image illustration of one having anxiety"
-                height="180"
-                />
-                <div class="card-body">
-                <h5 class="card-title">Your Anxiety Level</h5>
-                <p class="card-text">
-                    We provide the Generalized Anxiety Disorder 7 (GAD-7), which is
-                    a self-reported questionnaire for screening and severity
-                    measuring of anxiety.
-                </p>
-                <p class="card-text">
-                    You can get a valid and reliable measurement of your anxiety
-                    level within a minute and make informed decisions based on that.
-                </p>
-                <p class="card-text">
-                    Please seek professional support if your test results indicate
-                    high levels of anxiety.
-                </p>
-                </div>
-            </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="card" style="width: 18rem">
-                <img
-                src="./assets/learn_more_depression.jpg"
-                class="card-img-top"
-                alt="Image illustration of one having depression"
-                height="180"
-                />
-                <div class="card-body">
-                <h5 class="card-title">Your Depression Level</h5>
-                <p class="card-text">
-                    We provide the nine-item Patient Health Questionnaire (PHQ-9), a
-                    depressive symptom scale and diagnostic tool. It has been used
-                    by primary care providers to screen for possible depression.
-                </p>
-                <p class="card-text">
-                    Enjoy the convenience of this short scale and rest assured with
-                    its excellent validity and reliability.
-                </p>
-                <p class="card-text">
-                    Please seek professional support if your test results indicate
-                    high levels of depression.
-                </p>
-                </div>
-            </div>
-            </div>
-        </div>
-    `;
-
-  contactInfo.innerHTML = "";
 }
