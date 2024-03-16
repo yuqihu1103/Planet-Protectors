fa658c7dd08dd5677d775776db730071050366b7
yuqihu1103 <hu.yuqi@northeastern.edu>
2024-03-16 11:32:56 -0700

    back button; tree planter

--- public/js/game.js
+++ public/js/game.js
@@ -2,27 +2,34 @@
 document.addEventListener("DOMContentLoaded", function () {
   var buttonsContainer = document.getElementById("buttonsContainer");
   buttonsContainer.innerHTML = `
-    <button onclick="loadLevel(1)">Level 1</button>
-    <button onclick="loadLevel(2)">Level 2</button>
+    <button onclick="loadLevel(1)">Tree Planter</button>
+    <button onclick="loadLevel(2)">Level 1</button>
     <button onclick="loadLevel(3)">Level 3</button>
+    <button id="back"> Back to Homepage</button>
   `;
+  const backButton = document.getElementById("back");
+  backButton.addEventListener("click", redirectToHomePage);
+
+  function redirectToHomePage(event) {
+    event.preventDefault(); // Prevent default link behavior (navigating to "#")
+    window.location.href = "../index.html"; // Navigate to the game.html page
+  }
 });
 
 function loadLevel(level) {
   var scratchFrame = document.getElementById("scratchFrame");
   switch (level) {
     case 1:
-      scratchFrame.src = "https://scratch.mit.edu/projects/level1_link/embed";
+      scratchFrame.src = "https://scratch.mit.edu/projects/984039284/embed";
       break;
     case 2:
-      console.log("case 2 reached");
-      scratchFrame.src = "https://scratch.mit.edu/projects/level2_link/embed";
+      scratchFrame.src = "https://scratch.mit.edu/projects/level1_link/embed";
       break;
     case 3:
       scratchFrame.src = "https://scratch.mit.edu/projects/level3_link/embed";
       break;
     default:
-      scratchFrame.src = "https://scratch.mit.edu/projects/164932339/embed";
+      scratchFrame.src = "https://scratch.mit.edu/projects/984039284/embed";
       break;
   }
 }
